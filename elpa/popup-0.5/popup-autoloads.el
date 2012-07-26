ca3f79b51b1b2de12f4610a2435edc27290897ec
Aanjhan Ranganathan <aanjhan@gmail.com>
2012-07-27 01:05:40 +0200

    my dot emacs dot d

---
+++ elpa/popup-0.5/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (20437 60823
+;;;;;;  329588))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here

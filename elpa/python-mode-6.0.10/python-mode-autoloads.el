3b6792dabb7211f019a2a4b44c2d9df522aeb9ff
Aanjhan Ranganathan <aanjhan@gmail.com>
2014-05-15 09:55:26 +0200

    ...

--- elpa/python-mode-6.0.10/python-mode-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; python-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
-;;;;;;  (20651 43348 430341))
-
-;;;***
-
-(provide 'python-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; python-mode-autoloads.el ends here
